fa4d9aca3ce3e843216af57f4e3dbf002bfb59e9
Marcelo Barone <Marcelo.Barone@epicor.com>
2024-06-07 15:26:40 -0400

    Initial upload

---
+++ TestApp/MainForm.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace TestApp {
+    partial class MainForm {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (this.components != null)) {
+                this.components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            jiggleTimer = new System.Windows.Forms.Timer(components);
+            flpLayout = new FlowLayoutPanel();
+            panelBase = new Panel();
+            cmdAbout = new Button();
+            cmdTrayify = new Button();
+            cbSettings = new CheckBox();
+            cbJiggling = new CheckBox();
+            panelSettings = new Panel();
+            lbPeriod = new Label();
+            tbPeriod = new TrackBar();
+            cbMinimize = new CheckBox();
+            cbZen = new CheckBox();
+            niTray = new NotifyIcon(components);
+            flpLayout.SuspendLayout();
+            panelBase.SuspendLayout();
+            panelSettings.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)tbPeriod).BeginInit();
+            SuspendLayout();
+            // 
+            // jiggleTimer
+            // 
+            jiggleTimer.Interval = 1000;
+            jiggleTimer.Tick += jiggleTimer_Tick;
+            // 
+            // flpLayout
+            // 
+            flpLayout.AutoSize = true;
+            flpLayout.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            flpLayout.Controls.Add(panelBase);
+            flpLayout.Controls.Add(panelSettings);
+            flpLayout.Dock = DockStyle.Fill;
+            flpLayout.FlowDirection = FlowDirection.TopDown;
+            flpLayout.Location = new Point(0, 0);
+            flpLayout.Margin = new Padding(3, 4, 3, 4);
+            flpLayout.Name = "flpLayout";
+            flpLayout.Padding = new Padding(6, 7, 6, 7);
+            flpLayout.Size = new Size(347, 213);
+            flpLayout.TabIndex = 2;
+            // 
+            // panelBase
+            // 
+            panelBase.Controls.Add(cmdAbout);
+            panelBase.Controls.Add(cmdTrayify);
+            panelBase.Controls.Add(cbSettings);
+            panelBase.Controls.Add(cbJiggling);
+            panelBase.Location = new Point(9, 11);
+            panelBase.Margin = new Padding(3, 4, 3, 4);
+            panelBase.Name = "panelBase";
+            panelBase.Size = new Size(330, 37);
+            panelBase.TabIndex = 3;
+            // 
+            // cmdAbout
+            // 
+            cmdAbout.Location = new Point(226, 3);
+            cmdAbout.Margin = new Padding(3, 4, 3, 4);
+            cmdAbout.Name = "cmdAbout";
+            cmdAbout.Size = new Size(46, 31);
+            cmdAbout.TabIndex = 2;
+            cmdAbout.Text = "?";
+            cmdAbout.UseVisualStyleBackColor = true;
+            cmdAbout.Click += cmdAbout_Click;
+            // 
+            // cmdTrayify
+            // 
+            cmdTrayify.Location = new Point(279, 3);
+            cmdTrayify.Margin = new Padding(3, 4, 3, 4);
+            cmdTrayify.Name = "cmdTrayify";
+            cmdTrayify.Size = new Size(46, 31);
+            cmdTrayify.TabIndex = 3;
+            cmdTrayify.Text = "🔽";
+            cmdTrayify.UseVisualStyleBackColor = true;
+            cmdTrayify.Click += cmdTrayify_Click;
+            // 
+            // cbSettings
+            // 
+            cbSettings.Location = new Point(101, 7);
+            cbSettings.Margin = new Padding(3, 4, 3, 4);
+            cbSettings.Name = "cbSettings";
+            cbSettings.Size = new Size(88, 25);
+            cbSettings.TabIndex = 1;
+            cbSettings.Text = "Settings...";
+            cbSettings.UseVisualStyleBackColor = true;
+            cbSettings.CheckedChanged += cbSettings_CheckedChanged;
+            // 
+            // cbJiggling
+            // 
+            cbJiggling.AutoSize = true;
+            cbJiggling.Location = new Point(11, 7);
+            cbJiggling.Margin = new Padding(3, 4, 3, 4);
+            cbJiggling.Name = "cbJiggling";
+            cbJiggling.Size = new Size(90, 24);
+            cbJiggling.TabIndex = 0;
+            cbJiggling.Text = "Jiggling?";
+            cbJiggling.UseVisualStyleBackColor = true;
+            cbJiggling.CheckedChanged += cbJiggling_CheckedChanged;
+            // 
+            // panelSettings
+            // 
+            panelSettings.AutoSize = true;
+            panelSettings.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            panelSettings.Controls.Add(lbPeriod);
+            panelSettings.Controls.Add(tbPeriod);
+            panelSettings.Controls.Add(cbMinimize);
+            panelSettings.Controls.Add(cbZen);
+            panelSettings.Location = new Point(9, 56);
+            panelSettings.Margin = new Padding(3, 4, 3, 4);
+            panelSettings.Name = "panelSettings";
+            panelSettings.Size = new Size(330, 143);
+            panelSettings.TabIndex = 2;
+            panelSettings.Visible = false;
+            // 
+            // lbPeriod
+            // 
+            lbPeriod.AutoSize = true;
+            lbPeriod.Location = new Point(279, 55);
+            lbPeriod.Name = "lbPeriod";
+            lbPeriod.Size = new Size(27, 20);
+            lbPeriod.TabIndex = 3;
+            lbPeriod.Text = "1 s";
+            // 
+            // tbPeriod
+            // 
+            tbPeriod.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            tbPeriod.Location = new Point(5, 83);
+            tbPeriod.Margin = new Padding(3, 4, 3, 4);
+            tbPeriod.Maximum = 60;
+            tbPeriod.Minimum = 1;
+            tbPeriod.Name = "tbPeriod";
+            tbPeriod.Size = new Size(321, 56);
+            tbPeriod.TabIndex = 6;
+            tbPeriod.TickFrequency = 2;
+            tbPeriod.Value = 1;
+            tbPeriod.ValueChanged += tbPeriod_ValueChanged;
+            // 
+            // cbMinimize
+            // 
+            cbMinimize.AutoSize = true;
+            cbMinimize.Location = new Point(11, 49);
+            cbMinimize.Margin = new Padding(3, 4, 3, 4);
+            cbMinimize.Name = "cbMinimize";
+            cbMinimize.Size = new Size(153, 24);
+            cbMinimize.TabIndex = 5;
+            cbMinimize.Text = "Minimize on start?";
+            cbMinimize.UseVisualStyleBackColor = true;
+            cbMinimize.CheckedChanged += cbMinimize_CheckedChanged;
+            // 
+            // cbZen
+            // 
+            cbZen.AutoSize = true;
+            cbZen.Location = new Point(11, 15);
+            cbZen.Margin = new Padding(3, 4, 3, 4);
+            cbZen.Name = "cbZen";
+            cbZen.Size = new Size(105, 24);
+            cbZen.TabIndex = 4;
+            cbZen.Text = "Zen jiggle?";
+            cbZen.UseVisualStyleBackColor = true;
+            cbZen.CheckedChanged += cbZen_CheckedChanged;
+            // 
+            // niTray
+            // 
+            niTray.Icon = (Icon)resources.GetObject("niTray.Icon");
+            niTray.Text = "Test App";
+            niTray.DoubleClick += niTray_DoubleClick;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            ClientSize = new Size(347, 213);
+            Controls.Add(flpLayout);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 4, 3, 4);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "MainForm";
+            Text = "Test App";
+            Load += MainForm_Load;
+            Shown += MainForm_Shown;
+            flpLayout.ResumeLayout(false);
+            flpLayout.PerformLayout();
+            panelBase.ResumeLayout(false);
+            panelBase.PerformLayout();
+            panelSettings.ResumeLayout(false);
+            panelSettings.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)tbPeriod).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer jiggleTimer;
+        private System.Windows.Forms.FlowLayoutPanel flpLayout;
+        private System.Windows.Forms.Panel panelSettings;
+        private System.Windows.Forms.TrackBar tbPeriod;
+        private System.Windows.Forms.CheckBox cbMinimize;
+        private System.Windows.Forms.CheckBox cbZen;
+        private System.Windows.Forms.Panel panelBase;
+        private System.Windows.Forms.CheckBox cbSettings;
+        private System.Windows.Forms.CheckBox cbJiggling;
+        private System.Windows.Forms.Label lbPeriod;
+        private System.Windows.Forms.Button cmdAbout;
+        private System.Windows.Forms.NotifyIcon niTray;
+        private System.Windows.Forms.Button cmdTrayify;
+    }
+}
+
